3cd90b46f9b87f269b531e8c468c7f1d7fbaedc8
ENX1038 <149946487+EN1038@users.noreply.github.com>
2024-12-05 20:30:08 +0700

    up+

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -159,7 +159,7 @@ const Navbar = () => {
               <li className="flex items-center gap-2 px-4 py-2 hover:bg-gray-900">
                 <HomeIcon className="w-5 h-5 text-gray-200" />
                 <Link
-                  href="/login"
+                  href="/"
                   className="text-gray-200 hover:text-orange-400 duration-300 flex-grow"
                 >
                   Home
