4b798953afeff25ecffc14c0d03afb57ea1d10f1
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2024-12-11 09:42:56 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true, // ใช้ app directory (ถ้าคุณใช้ App Router)
-  },
   reactStrictMode: true, // เปิด react strict mode
-  swcMinify: true, // เปิดการใช้ SWC minify สำหรับการลดขนาดไฟล์
+  experimental: {
+    // ลบ appDir หากไม่ได้ใช้ App Router
+    appDir: false, 
+  },
 };
 
 export default nextConfig;
